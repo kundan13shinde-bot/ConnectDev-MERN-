92ef48fa06845dafb3a1418b84c12ce5622f217c
Kundan Shinde <kundan13shinde@gmail.com>
2026-07-04 15:09:35 +0530

    Fixed

--- ConnectDev-backend/src/routes/auth.js
+++ ConnectDev-backend/src/routes/auth.js
@@ -27,9 +27,12 @@ authRouter.post("/signup", async (req, res) => {
     const savedUser = await user.save();
     const token = await savedUser.getJWT();
 
-    res.cookie("token", token, {
-      expires: new Date(Date.now() + 8 * 3600000),
-    });
+res.cookie("token", token, {
+  expires: new Date(Date.now() + 8 * 3600000),
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+});
 
     res.json({ message: "User Added successfully!", data: savedUser });
   } catch (err) {
@@ -50,9 +53,12 @@ authRouter.post("/login", async (req, res) => {
     if (isPasswordValid) {
       const token = await user.getJWT();
 
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 3600000),
-      });
+   res.cookie("token", token, {
+  expires: new Date(Date.now() + 8 * 3600000),
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+});
       res.send(user);
     } else {
       throw new Error("Invalid credentials");
@@ -63,9 +69,13 @@ authRouter.post("/login", async (req, res) => {
 });
 
 authRouter.post("/logout", async (req, res) => {
-  res.cookie("token", null, {
-    expires: new Date(Date.now()),
-  });
+  
+res.cookie("token", null, {
+  expires: new Date(Date.now()),
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+});
   res.send("Logout Successful!!");
 });
 
